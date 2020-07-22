4aed29b36991b0e9558911237290b8e1f11e1fbf
Jumpei Ogawa <phanective@gmail.com>
2020-07-23 02:41:30 +0900

    feat: add deduplicate() & sleep()

---
+++ src/utils.ts
@@ -0,0 +1,22 @@
+/**
+ * Remove duplicate values from array.
+ *
+ * @param {T[]} arr - An array to deduplicate.
+ * @returns {T[]} - The deduplicated array.
+ * @template T
+ */
+export function deduplicate<T>(arr: T[]): T[] {
+  return arr.filter((el, i) => arr.indexOf(el) === i);
+}
+
+/**
+ * Sleep for the specified time.
+ *
+ * @param {number} ms - The time to wait (in milliseconds).
+ * @returns {Promise<void>} A promise object.
+ */
+export async function sleep(ms: number): Promise<void> {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(), ms);
+  });
+}
---
+++ test/utils.test.ts
@@ -0,0 +1,38 @@
+import { deduplicate, sleep } from "../src/utils";
+
+test("deduplicate", () => {
+  const deduplicated = deduplicate([
+    "www.example.com",
+    "www.example.org",
+    "www.example.net",
+    "www.example.io",
+
+    "www.example.net",
+    "www.example.com",
+    "www.example.net",
+    "www.example.net",
+    "www.example.net",
+    "www.example.org",
+    "www.example.io",
+    "www.example.org",
+    "www.example.net",
+    "www.example.org",
+    "www.example.io",
+  ]);
+
+  expect(deduplicated).toStrictEqual([
+    "www.example.com",
+    "www.example.org",
+    "www.example.net",
+    "www.example.io",
+  ]);
+});
+
+test("sleep", async () => {
+  const startAt = new Date();
+  await sleep(5000);
+  const endAt = new Date();
+
+  expect(+endAt - +startAt).toBeGreaterThan(4900);
+  expect(+endAt - +startAt).toBeLessThan(5100);
+});

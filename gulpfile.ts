ecd887b18435d3c80042174bfd8b1d1e69d7df05
Jumpei Ogawa <phanective@gmail.com>
2020-07-24 04:01:02 +0900

    feat: add support for both CommonJS & JSM

---
+++ gulpfile.ts
@@ -0,0 +1,26 @@
+import { dest, parallel } from "gulp";
+import * as ts from "gulp-typescript";
+import * as rename from "gulp-rename";
+
+exports.build = parallel(
+  () => {
+    const tsProject = ts.createProject("tsconfig.json", { module: "ES2015" });
+
+    return tsProject
+      .src()
+      .pipe(tsProject())
+      .js
+      .pipe(rename({ extname: ".mjs" }))
+      .pipe(dest("dist"));
+  },
+  () => {
+    const tsProject = ts.createProject("tsconfig.json", { module: "commonjs" });
+
+    return tsProject
+      .src()
+      .pipe(tsProject())
+      .js
+      .pipe(rename({ extname: ".cjs" }))
+      .pipe(dest("dist"));
+  },
+);

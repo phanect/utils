39b908bbc0f93f209c37505d2475148e658af51e
Jumpei Ogawa <git@phanective.org>
2023-12-26 23:21:08 +0900

    style: use arrow functions

--- src/utils.ts
+++ src/utils.ts
@@ -4,17 +4,14 @@
  * @returns {T[]} - The deduplicated array.
  * @template T
  */
-export function deduplicate<T>(arr: T[]): T[] {
-  return arr.filter((el, i) => arr.indexOf(el) === i);
-}
+export const deduplicate = <T>(arr: T[]): T[] => arr.filter((el, i) => arr.indexOf(el) === i);
 
 /**
  * Sleep for the specified time.
  * @param {number} ms - The time to wait (in milliseconds).
  * @returns {Promise<void>} A promise object.
  */
-export async function sleep(ms: number): Promise<void> {
-  return new Promise((resolve) => {
+export const sleep = async (ms: number): Promise<void> =>
+  new Promise((resolve) => {
     setTimeout(() => resolve(), ms);
   });
-}

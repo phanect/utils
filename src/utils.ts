d6528c3a854618b809750082051773b6bd407862
Jumpei Ogawa <git@phanective.org>
2023-12-31 15:13:03 +0900

    feat: add `sortObjects()`

--- src/utils.ts
+++ src/utils.ts
@@ -15,3 +15,31 @@ export const sleep = async (ms: number): Promise<void> =>
   new Promise((resolve) => {
     setTimeout(() => resolve(), ms);
   });
+
+/**
+ * Sort array of the objects by the same order.
+ * @param {object[]} objects - Aarray of objects.
+ * @returns {object[]} - Sorted array of the objects.
+ */
+export const sortObjects = <T extends { [key: string]: unknown }>(objects: T[]): T[] => objects.sort((obj1, obj2) => {
+  type Comparable = string|number|boolean|Date;
+
+  const keys = Object.keys(obj1).sort();
+
+  for (const key of keys) {
+    if (
+      (!(key in obj1) || !(key in obj2)) ||
+      obj1[key] === obj2[key]
+    ) {
+      continue;
+    } else if ((obj1[key] as Comparable) < (obj2[key] as Comparable)) { // if the values are not comparable, false is returned.
+      return -1;
+    } else if ((obj1[key] as Comparable) > (obj2[key] as Comparable)) { // if the values are not comparable, false is returned.
+      return 1;
+    } else {
+      continue;
+    }
+  }
+
+  return 0;
+});

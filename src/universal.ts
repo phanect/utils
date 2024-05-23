c5176f6989302217c4e32f2749d5e02f42b2a306
Jumpei Ogawa <git@phanective.org>
2024-05-24 06:49:59 +0900

    feat: add `modifiable()` for `array.map()`

--- src/universal.ts
+++ src/universal.ts
@@ -1,6 +1,7 @@
 export { deduplicate } from "./universal/deduplicate.ts";
 export { trimLines } from "./universal/trim-lines.ts";
 export { getLastElementOf } from "./universal/get-last-element-of.ts";
+export { modifiable } from "./universal/modifiable.ts";
 export { npmPackageExists } from "./universal/npm-package-exists.ts";
 export { random } from "./universal/random.ts";
 export { sleep } from "./universal/sleep.ts";

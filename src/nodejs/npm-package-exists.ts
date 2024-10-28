a6b6069ff658a0066ac6c52ddc7e2619ab76c5db
Jumpei Ogawa <git@phanective.org>
2024-10-28 19:36:48 +0900

    feat: add `npmPackageExists()` function

---
+++ src/nodejs/npm-package-exists.ts
@@ -0,0 +1,28 @@
+import { trimLines } from "../universal.js";
+
+export const npmPackageExists = async (pkgName: string): Promise<boolean> => {
+  const res = await fetch(`https://registry.npmjs.com/${ pkgName }`);
+
+  if (res.status < 400) {
+    return true;
+  } else if (res.status === 404) {
+    return false;
+  } else if (400 <= res.status && res.status < 500) {
+    throw new Error(trimLines(`
+      [ERROR] Package name "${pkgName}" may be invalid.
+      Could not check if npm package  exists.
+
+      HTTP ${res.status} ${res.statusText} https://registry.npmjs.com/${pkgName}
+    `));
+  } else if (500 <= res.status) {
+    throw new Error(trimLines(`
+      [ERROR] registry.npmjs.com may be down. See: https://status.npmjs.org
+      Could not check if npm package "${pkgName}" exists.
+      HTTP ${res.status} ${res.statusText} https://registry.npmjs.com/${pkgName}
+    `));
+  } else {
+    throw new Error(trimLines(`
+      [ERROR] Unexpected HTTP Status Code ${res.status} ${res.statusText} while checking https://registry.npmjs.com/${pkgName}
+    `));
+  }
+};

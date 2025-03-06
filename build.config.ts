8abb4cbe990117907ad3f098b267fcf687777cf8
Jumpei Ogawa <git@phanective.org>
2025-03-06 14:59:05 +0900

    chore: replace unbuild with tsdown

--- build.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineBuildConfig } from "unbuild";
-
-export default defineBuildConfig({
-  entries: ["./src/universal.ts", "./src/nodejs.ts"],
-  declaration: true,
-  rollup: {
-    emitCJS: true,
-  },
-});

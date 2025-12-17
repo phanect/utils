b3c33b2e557d4690b50bf3ce26c3a9417b9fee02
Jumpei Ogawa <git@phanective.org>
2025-12-18 08:14:02 +0900

    fix(pseudoRandomInt): max returning number was `max -1`

--- src/universal/pseudo-random-int.ts
+++ src/universal/pseudo-random-int.ts
@@ -3,4 +3,4 @@
  * @param max - max number of the generated random number
  * @returns random number (integer)
  */
-export const pseudoRandomInt = (max: number): number => Math.floor(Math.random() * max);
+export const pseudoRandomInt = (max: number): number => Math.floor(Math.random() * (max + 1));

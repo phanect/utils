efd7fd08ca174a0c12b399447f7ad99bedc100ff
Jumpei Ogawa <git@phanective.org>
2025-12-18 07:02:56 +0900

    feat: rename `random()` to `pseudoRandomInt()` [BREAKING]

--- src/universal.ts
+++ src/universal.ts
@@ -2,7 +2,7 @@ export { deduplicate } from "./universal/deduplicate.ts";
 export { trimLines } from "./universal/trim-lines.ts";
 export { getLastElementOf } from "./universal/get-last-element-of.ts";
 export { npmPackageExists } from "./universal/npm-package-exists.ts";
-export { random } from "./universal/random.ts";
+export { pseudoRandomInt } from "./universal/pseudo-random-int.ts";
 export { sleep } from "./universal/sleep.ts";
 export { sortObjects } from "./universal/sort-objects.ts";
 

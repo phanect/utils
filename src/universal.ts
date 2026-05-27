aa9f7a5dcd8a5025e07c8b8e2f2853a0303e45e5
Jumpei Ogawa <git@phanective.org>
2026-05-27 14:16:41 +0900

    feat: remove `sleep()`
    Replace it with `es-toolkit`'s `delay()`.

--- src/universal.ts
+++ src/universal.ts
@@ -4,5 +4,4 @@ export { getLastElementOf } from "./universal/get-last-element-of.ts";
 export { npmPackageExists } from "./universal/npm-package-exists.ts";
 export { pseudoRandomInt } from "./universal/pseudo-random-int.ts";
 export { pseudoRandomString } from "./universal/pseudo-random-string.ts";
-export { sleep } from "./universal/sleep.ts";
 export { sortObjects } from "./universal/sort-objects.ts";
--- src/universal/sleep.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Sleep for the specified time.
- * @param ms - The time to wait (in milliseconds).
- * @returns A promise object.
- */
-export const sleep = async (ms: number): Promise<void> =>
-  new Promise((resolve) => {
-    setTimeout(() => resolve(), ms);
-  });
